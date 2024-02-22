41a8974d6131357bf11975cd1e86fecdf038f9da
Thomas Mumena <mumenathomas@gmail.com>
2024-02-22 18:54:44 +0200

    Get the timer to display in the 'm:s' format gracefully, both for single and double  values for 'seconds'.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -17,12 +18,21 @@ def reset_counter():
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_countdown():
-    pass
+    counter = 60 * 3
+    count_down(counter)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(counter):
-    canvas.itemconfig(timer_text, counter)
+    min_count = math.floor(counter / 60)
+    sec_count = counter % 60
+    # if min_count >= 0 and sec_count < 10:
+    #     sec_count = f'0{sec_count}'
+    # if min_count > 0 and sec_count == 0:
+    #     sec_count = '00'
+    if sec_count < 10:
+        sec_count = f'0{sec_count}'
+    canvas.itemconfig(timer_text, text=f'{min_count}:{sec_count}')
     if counter > 0:
         window.after(1000, count_down, counter - 1)
 
@@ -45,7 +55,7 @@ timer_label.grid(column=1, row=0)
 check_mark_label = Label(text="✓", bg=YELLOW, fg=GREEN)
 check_mark_label.grid(column=1, row=3)
 
-start_button = Button(text='Start', highlightthickness=0)
+start_button = Button(text='Start', highlightthickness=0, command=start_countdown)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text='Reset', highlightthickness=0)
