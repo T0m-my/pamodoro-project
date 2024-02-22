dbe9a8216d99e5f89d7c9cc5d6cb31497e00474d
Thomas Mumena <mumenathomas@gmail.com>
2024-02-22 11:37:11 +0200

    Change the UI background to a tomato canvas. Setup it properly and add some properly formatted text.

--- main.py
+++ main.py
@@ -17,7 +17,14 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-
 window.title('Pomodoro')
+window.config(padx=100, pady=50, bg=YELLOW)
+
+tomato_img = PhotoImage(file='./tomato.png')
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.pack()
 
 window.mainloop()
